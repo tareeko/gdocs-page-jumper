eca3dcfb7a93077720a5c8c17252e28f171e62fb
tareeko <etarek@gmail.com>
2023-09-13 12:49:14 -0400

    Version 1.2 - revision to work with gdocs update

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,34 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  var pageArray = document.querySelectorAll(".kix-page");
-  var data = request.data;
-  if (data <= (pageArray.length)) {
-    document.querySelectorAll('.kix-page')[data - 1].scrollIntoView(true);
-    sendResponse({
-      data: data,
-      success: true
-    });
-  } else {
-    sendResponse({
-      status: status
-    });
-  }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  let editor = document.querySelector('.kix-appview-editor');
+  let scroller = document.querySelector('.jfk-tooltip-contentId');
+  let docLength = document.querySelector(
+    '.kix-rotatingtilemanager'
+  ).scrollHeight;
+
+  let currentScroll = editor.scrollTop;
+  editor.scrollTop = docLength;
+  setTimeout(() => {
+    editor.scrollTop = currentScroll;
+  }, 25);
+
+  let totalPages = 1;
+
+  setTimeout(() => {
+    totalPages = scroller.textContent.split(' ')[2];
+    let pageFactor = docLength / totalPages;
+
+    var data = request.data;
+
+    if (totalPages > 1) {
+      editor.scrollTop = (data - 1) * pageFactor;
+      sendResponse({
+        data: data,
+        success: true,
+      });
+    } else {
+      sendResponse({
+        status: status,
+      });
+    }
+  }, 50);
 });
