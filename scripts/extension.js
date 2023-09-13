212eaae561e4f8e26bc61bdaebeb9c4c904fffa9
tareeko <etarek@gmail.com>
2023-09-13 13:45:28 -0400

    Implement field autofocus, page close, enter key

--- scripts/extension.js
+++ scripts/extension.js
@@ -1,15 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('status').textContent = "Extension loaded";
+document.addEventListener('DOMContentLoaded', function () {
   var button = document.getElementById('jump-button');
-  button.addEventListener('click', function() {
-    var page = $('#page-input').val();
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        data: page
-      }, function(response) {});
-    });
+  var pageInput = document.getElementById('page-input');
+  document.getElementById('status').textContent = 'Extension loaded';
+
+  button.addEventListener('click', function () {
+    jumpToPage();
   });
+  pageInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      jumpToPage();
+    }
+  });
+
+  function jumpToPage() {
+    var page = pageInput.value;
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      function (tabs) {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          { data: page },
+          function (response) {
+            window.close();
+          }
+        );
+      }
+    );
+  }
 });
